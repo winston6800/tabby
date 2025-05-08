2bd0f08a3f811f05ea04975759deffd2e735bb4c
Jack12404 <jackli@computingforall.org>
2025-05-07 19:50:30 -0700

    updated the comments on each database

--- server/src/database.ts
+++ server/src/database.ts
@@ -2,12 +2,14 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./tabby.db');
 
 db.serialize(()=> {
+    //Users database for logins
     db.run(`CREATE TABLE IF NOT EXISTS users (
         userId INTEGER PRIMARY KEY AUTOINCREMENT, 
         userName TEXT,
         password TEXT
         )`); 
-
+    
+    //nodes database
     db.run(`CREATE TABLE IF NOT EXISTS nodes (
         nodeId INTEGER PRIMARY KEY AUTOINCREMENT,
         nodeName TEXT,
@@ -15,7 +17,8 @@ db.serialize(()=> {
         createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
         FOREIGN KEY (userId) REFERENCES users(userId) ON DELETE CASCADE
         )`); 
-
+    
+    //edges database
     db.run(`CREATE TABLE IF NOT EXISTS edges (
         edgeId INTEGER PRIMARY KEY AUTOINCREMENT,
         fromNodeId INTEGER,
